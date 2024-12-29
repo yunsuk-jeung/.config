f7dd33f7d89a0aec081605f9522de32ac578f3e1
ysjeung <yunsukjeung@snu.ac.kr>
2024-12-30 01:16:31 +0900

    chagne tele to fzf

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -1,8 +1,20 @@
 return {
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- or if using mini.icons/mini.nvim
-  -- dependencies = { "echasnovski/mini.icons" },
-  opts = {}
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- or if using mini.icons/mini.nvim
+	-- dependencies = { "echasnovski/mini.icons" },
+	opts = {},
+	vim.keymap.set("n", "<leader>ff", function()
+		require("fzf-lua").files()
+	end, { desc = "Find Files" }),
+	vim.keymap.set("n", "<leader>fg", function()
+		require("fzf-lua").live_grep()
+	end, { desc = "Live Grep" }),
+	vim.keymap.set("n", "<leader>fb", function()
+		require("fzf-lua").buffers()
+	end, { desc = "Buffers" }),
+	vim.keymap.set("n", "<leader>fh", function()
+		require("fzf-lua").help_tags()
+	end, { desc = "Help Tags" }),
 }
