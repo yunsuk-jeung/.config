3ea4a74f714fa5954a3735c1cada1d6b054b0d53
ysjeung <yunsukjeung@snu.ac.kr>
2025-02-09 02:52:47 +0900

    change keymap

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -9,7 +9,7 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 local opts = { noremap = true, silent = true }
 
 -- save file without auto-formatting
-vim.keymap.set('n', '<C-s>', '<cmd>noautocmd w <CR>', opts)
+vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', opts)
 vim.keymap.set('i', '<C-s>', '<cmd> w<CR><Esc>', opts)
 -- quit file
 vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts)
